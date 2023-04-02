315adce033163a8e72372d7ebb1d696fd4699be0
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-04-02 19:44:01 +0200

    Update MSDOORS.lua

--- MSDOORS.lua
+++ MSDOORS.lua
@@ -2,11 +2,11 @@ local s,e = pcall(function()
     local function getexploit()
         return (identifyexecutor and table.concat({ identifyexecutor() }, " ")) or ("Unknown")
     end
-    local isMobile = false
+  --[[  local isMobile = false
     local mobiletoggles,mobiletoggleerr=pcall(function()
         local platform = UserInputService:GetPlatform()
         isMobile = (platform == Enum.Platform.Android or platform == Enum.Platform.IOS)
-    end)
+    end)--]]
     local exploit = getexploit()
     if exploit == "Fluxus Android" then 
         loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/Moonsec.lua"),true))()
