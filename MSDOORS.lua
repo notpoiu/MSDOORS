03c66028c4ce36961d54091c57854ac6b98534aa
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-04-03 15:21:19 +0200

    Update MSDOORS.lua

--- MSDOORS.lua
+++ MSDOORS.lua
@@ -1,21 +1,19 @@
-local s,e = pcall(function()
-    local isMobile = false
-    local mobiletoggles,mobiletoggleerr=pcall(function()
-        local platform = UserInputService:GetPlatform()
-        isMobile = (platform == Enum.Platform.Android or platform == Enum.Platform.IOS)
-    end)
-  
-    if isMobile == true then
-        print("MSDOORS: Loading mobile version...")
-        loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/Moonsec.lua"),true))()
-    else
-        print("MSDOORS: Loading PC version...")
-        loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/MSDOORS.lua"),true))()
-    end
+local isMobile = false
+local mobiletoggles,mobiletoggleerr = pcall(function()
+	local platform = game:GetService("UserInputService"):GetPlatform()
+	isMobile = (platform == Enum.Platform.Android or platform == Enum.Platform.IOS)
 end)
 
-if e then
-    print("MSDOORS: Failed to check executor loading main...")
-    print(e)
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/MSDOORS.lua"),true))()
+if mobiletoggleerr then
+	warn("MSDOORS: Failed to check executor, report this issue to mstudio45:")	
+	warn(mobiletoggleerr)
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/Moonsec.lua"),true))()
+else
+	if isMobile == true then
+		print("MSDOORS: Loading mobile version...")
+		loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/Moonsec.lua"),true))()
+	else
+		print("MSDOORS: Loading PC version...")
+		loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/MSDOORS.lua"),true))()
+	end
 end
