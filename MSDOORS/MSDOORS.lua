7bd97c2d956a775d683c2d7973d79715b30998ea
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-04-20 16:03:33 +0200

    Update MSDOORS.lua

--- MSDOORS/MSDOORS.lua
+++ MSDOORS/MSDOORS.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8a4e000fc318917bf08cd5b8e614b99c.lua"))()
+loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19069/MSDOORS.lua'))()
