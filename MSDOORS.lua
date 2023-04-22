d19c7213e2ef59b0ed58c7f8b4d88599de789dad
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-04-22 15:48:29 +0200

    Update MSDOORS.lua

--- MSDOORS.lua
+++ MSDOORS.lua
@@ -1,22 +1,7 @@
 print("MSDOORS: Loading...")
-loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19135/MSDOORS.lua'))()
-
---[[local isMobile = false
-local mobiletoggles,mobiletoggleerr = pcall(function()
-	local platform = game:GetService("UserInputService"):GetPlatform()
-	isMobile = (platform == Enum.Platform.Android or platform == Enum.Platform.IOS)
+game:GetService("GuiService").ErrorMessageChanged:Connect(function(a)
+	if(a:match("LDR")or a:match("LPH"))and a:match("Invalid HWID")then 
+		game.Players.LocalPlayer:Kick("MSDOORS: Your executor is not supported. Join the discord server (.gg/eeHV6Tsfwd)!")
+	end 
 end)
-
-if mobiletoggleerr then
-	warn("MSDOORS: Failed to check executor, report this issue to mstudio45:")	
-	warn(mobiletoggleerr)
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/Moonsec.lua"),true))()
-else
-	if isMobile == true then
-		print("MSDOORS: Loading mobile version...")
-		loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/Moonsec.lua"),true))()
-	else
-		print("MSDOORS: Loading PC version...")
-		loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSDOORS/MSDOORS.lua"),true))()
-	end
-end--]]
+loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19135/MSDOORS.lua'))()
