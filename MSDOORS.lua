6593d81716609f6dbc191eecbd090ee47d2c0276
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-04-30 20:35:02 +0200

    Update MSDOORS.lua

--- MSDOORS.lua
+++ MSDOORS.lua
@@ -1,2 +1,2 @@
 print("MSHUB: Loading...")
-loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19389/LoaderMSHUB.lua'))()
+loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19391/LoaderMSHUB.lua'))()
