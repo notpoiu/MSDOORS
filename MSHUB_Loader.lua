15113b9d78490a75a70b32a58e5172f32a7a2705
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-05-03 07:29:38 +0200

    Update MSHUB_Loader.lua

--- MSHUB_Loader.lua
+++ MSHUB_Loader.lua
@@ -1,2 +1,2 @@
 print("MSHUB: Loading...")
-loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19446/LoaderMSHub.lua'))()
+loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19455/Loader.lua'))()
