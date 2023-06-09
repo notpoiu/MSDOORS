d8c76fd9f64bf8a876e4c292204611c4ac33e991
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-06-09 15:16:06 +0200

    Update MSHUB_Loader.lua

--- MSHUB_Loader.lua
+++ MSHUB_Loader.lua
@@ -13,4 +13,4 @@ game:GetService("GuiService").ErrorMessageChanged:Connect(function(kickmsg)
 end)
 
 task.wait()
-loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19810/mshubLoaDer.lua'))()
+loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/19908/loaderms.lua'))()
