4303f57bbc0568446548a870f225e7aefd60fb3d
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-07-10 09:54:22 +0200

    Update JoinSquad.lua

--- Codes/Lobby/JoinSquad.lua
+++ Codes/Lobby/JoinSquad.lua
@@ -70,7 +70,7 @@ repeat
 	end
 	task.wait(1)
 	if teleported == false then
-		normalmessage("MSDOORS", "Squad elevators are full, retrying...", 2)
+		normalmessage("MSHUB", "Squad elevators are full, retrying...", 2)
 	end
 until teleported == true
-normalmessage("MSDOORS", "Joining...", 10)
+normalmessage("MSHUB", "Joining...", 10)
