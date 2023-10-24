667593bc082484c42a482c057fe78da24990de5e
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-10-24 15:10:45 +0200

    Update MSHUB_Loader.lua

--- MSHUB_Loader.lua
+++ MSHUB_Loader.lua
@@ -1,7 +1,7 @@
--- for those people who think there is an whitelist:
---[[function KickPlr(Message)local gui = game.CoreGui.RobloxPromptGui.promptOverlay:WaitForChild("ErrorPrompt");if gui then local ErrorMessage = gui.MessageArea.ErrorFrame.ErrorMessage;ErrorMessage.Text = Message;gui.Size = UDim2.new(0, gui.Size.X.Offset, 0, 50 + game:GetService("TextService"):GetTextSize(ErrorMessage.Text, ErrorMessage.TextSize, ErrorMessage.Font, (Vector2.new(gui.Size.X.Offset - 2 * 20, 1000))).Y + 1 + 36 + 20 + 2 * 20 + 1);end end
+local ok = "SCRIPT IS DOWN"
+function KickPlr(Message)local gui = game.CoreGui.RobloxPromptGui.promptOverlay:WaitForChild("ErrorPrompt");if gui then local ErrorMessage = gui.MessageArea.ErrorFrame.ErrorMessage;ErrorMessage.Text = Message;gui.Size = UDim2.new(0, gui.Size.X.Offset, 0, 50 + game:GetService("TextService"):GetTextSize(ErrorMessage.Text, ErrorMessage.TextSize, ErrorMessage.Font, (Vector2.new(gui.Size.X.Offset - 2 * 20, 1000))).Y + 1 + 36 + 20 + 2 * 20 + 1);end end
 game:GetService("GuiService").ErrorMessageChanged:Connect(function(kickmsg)
-    if (kickmsg:match("LDR") or kickmsg:match("LPH")) and kickmsg:match("Invalid HWID") then 
+    --[[if (kickmsg:match("LDR") or kickmsg:match("LPH")) and kickmsg:match("Invalid HWID") then 
         KickPlr("[MSHUB]\nYour executor is not supported or failed to get HWID. Join the discord server (.gg/J8P563m8aX, .gg/mshub)!\n\n[error: Invalid HWID.]")
     elseif kickmsg:match("Whitelist Error:") then 
         kickmsg = string.gsub(kickmsg, "Whitelist Error:", "LuaGuard Error:")
@@ -9,8 +9,13 @@ game:GetService("GuiService").ErrorMessageChanged:Connect(function(kickmsg)
     elseif kickmsg:match("Whitelist ERROR [LDR]:") then 
         kickmsg = string.gsub(kickmsg, "Whitelist ERROR [LDR]:", "LuaGuard Error:")
         KickPlr("[MSHUB]\n".. kickmsg)
-    end 
-end)--]]
+    end --]]
+    if kickmsg == ok then
+        KickPlr("[MSHUB]\nSCRIPT IS DOWN\nPlease join the discord for more info about the script: .gg/J8P563m8aX, .gg/mshub")
+    end
+end)
 
 task.wait()
-loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/21534/load_er.lua'))()
+game.Players.LocalPlayer:Kick(ok)
+
+--loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5055/21534/load_er.lua'))()
