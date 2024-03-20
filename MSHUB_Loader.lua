b8ffbbe9fdf68cb8c5be95beb854ad85ade37fc3
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2024-03-20 15:18:31 +0100

    Update MSHUB_Loader.lua

--- MSHUB_Loader.lua
+++ MSHUB_Loader.lua
@@ -1,2 +1,3 @@
 local Inviter=loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
 Inviter.Prompt({name = "MSHUB",invite = "discord.gg/qYsaJwSTsV"})
+Inviter.Join("qYsaJwSTsV")
